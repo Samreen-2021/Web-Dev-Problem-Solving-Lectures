eabc40910a7cfd8b933b816c045ca5b7618ffaab
samreen-2021 <determinedlearner2019@gmail.com>
2021-05-23 13:19:26 +0500

    Implementation and Practice of Javascript Functions.

--- Class-1/app.js
+++ Class-1/app.js
@@ -1,4 +1,9 @@
-
-const name ="farah";
-name="sarah";
-console.log(name);
+ function printVariable(name)
+ {
+     let name1 = name;
+     console.log(name1);
+ }
+ 
+// const name ="farah";
+// name="sarah";
+// console.log(name);
