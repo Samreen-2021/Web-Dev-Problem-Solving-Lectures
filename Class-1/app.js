7fe966b39710d3e55d4fdf746c52dc57ba1685f2
samreen-2021 <determinedlearner2019@gmail.com>
2021-05-23 13:44:49 +0500

    Implementation and Practice of Conditional statement in Javascript.

--- Class-1/app.js
+++ Class-1/app.js
@@ -1,9 +1,26 @@
+//  function printVariable(name)
+//  {
+//      let name1 = name;
+//      console.log(name1);
+//  }
  function printVariable(name)
  {
      let name1 = name;
-     console.log(name1);
+     if(name1=="abc"){
+         alert("true" )
+     };
  }
- 
+//  let name = prompt("name");
+
+// if(name=="abc")
+// {
+//     alert("true");
+// }
+// else{
+//     alert("false");  
+// }
+
+
 // const name ="farah";
 // name="sarah";
 // console.log(name);
