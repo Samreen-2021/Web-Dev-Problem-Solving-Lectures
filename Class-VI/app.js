03a71fa2cb70963e797a812706ed4606748ba37d
samreen-2021 <determinedlearner2019@gmail.com>
2021-06-06 23:16:27 +0500

    Implementation of CSS styling through Javascript completed.

---
+++ Class-VI/app.js
@@ -0,0 +1,30 @@
+
+// function showBorder(){
+
+// document.querySelector('h1').style.border=" dotted black 10px";
+// let element =document.querySelector('h1').style.backgroundColor= "pink";
+// console.log(element);
+// let item = document.querySelector('h1').className= "heading1";
+// let list = document.querySelector('h1').classList.add('abc');
+// console.log(list);
+
+// }
+document.getElementsByClassName("myBtn")[0].addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = "Hello World";});
+
+
+
+
+// var x = parseInt(document.querySelector('#numb1').value);
+// var y = parseInt(document.querySelector('#numb2').value);
+// function add(){
+//     let sum = x + y;
+// }
+// function sub(){
+//     let sub = x - y;
+// }
+// function multiply(){
+//     let product = x * y;
+// }
+// function divide(){
+//     let  divide = x / y;}
